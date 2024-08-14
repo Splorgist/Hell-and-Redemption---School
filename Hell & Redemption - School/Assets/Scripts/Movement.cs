27db91f9be8d46e571cf8fa14c8b47cbef5076f3
Splorgist <118773402+Splorgist@users.noreply.github.com>
2024-08-14 16:16:57 +1200

    Initial

---
+++ Hell & Redemption - School/Assets/Scripts/Movement.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGrounded;
+
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D)){
+            gameObject.transform.localScale = new Vector3(2, 1, 1);
+        }
+
+        if (Input.GetKey(KeyCode.A)){
+            gameObject.transform.localScale = new Vector3(-2, 1, 1);
+        }
+
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGrounded);
+
+        if(isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        if(Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if(jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            } else {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)){
+            isJumping = false;
+        }
+    }
+}
