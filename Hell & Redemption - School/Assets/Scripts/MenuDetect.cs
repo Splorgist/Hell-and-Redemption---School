39147ec5976b350a02397d1cc02babd8b9df3299
Splorgist <communisthing@gmail.com>
2024-09-05 14:39:16 +1200

    Menu_redoV1

--- Hell & Redemption - School/Assets/Scripts/MenuDetect.cs
+++ Hell & Redemption - School/Assets/Scripts/MenuDetect.cs
@@ -7,66 +7,56 @@ using UnityEngine.SceneManagement;
 
 public class MenuDetect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public GameObject menu;
-    public Animator animator;
-    public Animator image;
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
 
-    private bool isPointerOver = false;
-    public bool clickable = true;
-    public string scene;
 
-    private void Start()
-    {
-        menu.name = name;
-    }
+    public bool button1_down = false;
+    public bool button2_down = false;
+    public bool button3_down = false;
+    public bool button4_down = false;
 
-    private void Update()
-    {
-        if (isPointerOver && Input.GetMouseButtonDown(0) && menu.name == "Q")
-        {
-            Debug.Log("Exit");
-            animator.SetTrigger("deselect");
-            Cursor.lockState = CursorLockMode.Locked;
-            image.SetTrigger("fade_in");
-        }else if (isPointerOver && Input.GetMouseButtonDown(0) && menu.name == "O"){
-            Debug.Log("Options");
-            animator.SetTrigger("deselect");
-            Cursor.lockState = CursorLockMode.Locked;
-            image.SetTrigger("fade_in");
-        }else if (isPointerOver && Input.GetMouseButtonDown(0) && menu.name == "LG"){
-            Debug.Log("Load Game");
-            animator.SetTrigger("deselect");
-            Cursor.lockState = CursorLockMode.Locked;
-            image.SetTrigger("fade_in");
-        }else if (isPointerOver && Input.GetMouseButtonDown(0) && menu.name == "Start"){
-            Debug.Log("Start Game");
-            animator.SetTrigger("deselect");
-            Cursor.lockState = CursorLockMode.Locked;
-            image.SetTrigger("fade_in");
-            StartCoroutine (SceneChange());
-        }
-    }
+    public Animator anim1;
+    public Animator anim2;
+    public Animator anim3;
+    public Animator anim4;
+
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Debug.Log("Entered " + menu.name);
-        animator.SetTrigger("select");
-
-        isPointerOver = true;
+        Debug.Log("Pointer entered button: " + eventData.pointerEnter.name);
+        if (eventData.pointerEnter.name == button1.name){
+            Debug.Log("Play selected for button1");
+            anim1.Play("button1_select");
+        }else if (eventData.pointerEnter.name == button2.name){
+            Debug.Log("Play selected for button2");
+            anim2.Play("button2_select");
+        }else if (eventData.pointerEnter.name == button3.name){
+            Debug.Log("Play selected for button3");
+            anim3.Play("button3_select");
+        }else if (eventData.pointerEnter.name == button4.name){
+            Debug.Log("Play selected for button4");
+            anim4.Play("button4_select");
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        Debug.Log("Exited " + menu.name);
-        animator.SetTrigger("deselect");
-
-        isPointerOver = false;
-    }
-
-    private IEnumerator SceneChange()
-    {
-        yield return new WaitForSeconds(2);
-        Debug.Log("Scene Change");
-        SceneManager.LoadScene(scene);
+        Debug.Log("Pointer exited button: " + eventData.pointerEnter.name);
+        if (eventData.pointerEnter.name == button1.name){
+            Debug.Log("Play deselect for " + button1.name);
+            anim1.Play("button1_deselect");
+        }else if (eventData.pointerEnter.name == button2.name){
+            Debug.Log("Play deselect for " + button2.name);
+            anim2.Play("button2_deselect");
+        }else if (eventData.pointerEnter.name == button3.name){
+            Debug.Log("Play deselect for " + button3.name);
+            anim3.Play("button3_deselect");
+        }else if (eventData.pointerEnter.name == button4.name){
+            Debug.Log("Play deselect for button4" + button4.name);
+            anim4.Play("button4_deselect");
+        }
     }
 }
